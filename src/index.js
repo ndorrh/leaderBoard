f437cb2e75b3640f5f648cc7847fba9c94a7b0e3
ndorrh <ndorrh@gmail.com>
2022-10-12 22:14:41 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './style.css';
+import { sendData, getData } from './modules/methods.js';
 
-import { render } from './modules/scores.js';
-import { sendData, getData, tbody } from './modules/methods.js';
-
-//const tbody = document.querySelector('.tbody');
 const addScoreBtn = document.getElementById('submit-btn');
 const refreshBtn = document.getElementById('refresh');
-//tbody.innerHTML = render();
+
 addScoreBtn.addEventListener('click', (e) => {
   e.preventDefault();
   sendData();
