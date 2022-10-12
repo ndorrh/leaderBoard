cbceaeb9d24e16bae5bb25458ef9c1b484ffac61
ndorrh <ndorrh@gmail.com>
2022-10-12 21:37:36 +0100

    Modify scores.js

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,58 +1,15 @@
-const users = [
-  {
-    id: 1,
-    name: 'Player 1',
-    score: 570,
-  },
-
-  {
-    id: 2,
-    name: 'Player 2',
-    score: 300,
-  },
-
-  {
-    id: 3,
-    name: 'Player 3',
-    score: 700,
-  },
-
-  {
-    id: 4,
-    name: 'Player 4',
-    score: 1100,
-  },
-
-  {
-    id: 5,
-    name: 'Player 5 ',
-    score: 60,
-  },
-
-  {
-    id: 6,
-    name: 'Player 6 ',
-    score: 6000,
-  },
-
-  {
-    id: 7,
-    name: 'Player 7 ',
-    score: 6700,
-  },
-];
-
-const render = () => {
+const tbody = document.querySelector('.tbody');
+const render = (user) => {
   let tdata = '';
-  users.forEach((player, index) => {
+  user.forEach((player, index) => {
     tdata += `
                 <tr>
-                  <td>${users[index].id}</td>
-                  <td>${users[index].name}:</td>
-                  <td>${users[index].score}</td>
+                 <td>${index}:</td>
+                  <td>${user[index].user}:</td>
+                  <td>${user[index].score}</td>
                 </tr>`;
   });
   return tdata;
 };
 
-export { users, render };
+export { render, tbody };
