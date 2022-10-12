062cf6c54a26ffeeaca96ce3bf67acb48bbe3103
ndorrh <ndorrh@gmail.com>
2022-10-12 21:34:14 +0100

    Modify index.js by Adding event handler for the refresh and summit button and Call the getData function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './style.css';
 
 import { render } from './modules/scores.js';
+import { sendData, getData, tbody } from './modules/methods.js';
 
-const tbody = document.querySelector('.tbody');
-tbody.innerHTML = render();
+//const tbody = document.querySelector('.tbody');
+const addScoreBtn = document.getElementById('submit-btn');
+const refreshBtn = document.getElementById('refresh');
+//tbody.innerHTML = render();
+addScoreBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  sendData();
+  getData();
+});
+
+refreshBtn.addEventListener('click', () => {
+  getData();
+});
+
+getData();
