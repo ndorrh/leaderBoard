2db048a1c497d591197019801c99a9881dba1dbb
ndorrh <ndorrh@gmail.com>
2022-10-12 21:54:21 +0100

    Fix linter errors

---
+++ gameid.js
@@ -0,0 +1,13 @@
+/* fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+
+Adding method type
+method: 'POST',
+  headers: {
+  'Content-Type': 'application/json',
+  },
+Adding body or contents to send
+body: JSON.stringify({
+  name: 'oswald',
+}),
+}).then((good) => good.json()).then((err) => console.log(err));
+console.log('rest'); */
